3bfca13d52035437d32848de8d89077a14819d43
dtony <dtony@DESKTOP-MHDPD8D>
2022-08-25 18:30:56 -0500

    Add project files.

---
+++ VehicleReload/Helper.cs
@@ -0,0 +1,67 @@
+﻿using Base.Core;
+using Base.Defs;
+using PhoenixPoint.Common.Core;
+using System;
+using System.Linq;
+
+
+namespace VehicleReload
+{
+    internal class Helper
+    {
+        private static readonly DefRepository Repo = VehicleReloadMain.Repo;
+        private static readonly SharedData Shared = VehicleReloadMain.Shared;
+
+       
+        public static T CreateDefFromClone<T>(T source, string guid, string name) where T : BaseDef
+        {
+            DefRepository Repo = GameUtl.GameComponent<DefRepository>();
+            try
+            {
+                if (Repo.GetDef(guid) != null)
+                {
+                    if (!(Repo.GetDef(guid) is T tmp))
+                    {
+                        throw new TypeAccessException($"An item with the GUID <{guid}> has already been added to the Repo, but the type <{Repo.GetDef(guid).GetType().Name}> does not match <{typeof(T).Name}>!");
+                    }
+                    else
+                    {
+                        if (tmp != null)
+                        {
+                            return tmp;
+                        }
+                    }
+                }
+                T tmp2 = Repo.GetRuntimeDefs<T>(true).FirstOrDefault(rt => rt.Guid.Equals(guid));
+                if (tmp2 != null)
+                {
+                    return tmp2;
+                }
+                Type type = null;
+                string resultName = "";
+                if (source != null)
+                {
+                    int start = source.name.IndexOf('[') + 1;
+                    int end = source.name.IndexOf(']');
+                    string toReplace = !name.Contains("[") && start > 0 && end > start ? source.name.Substring(start, end - start) : source.name;
+                    resultName = source.name.Replace(toReplace, name);
+                }
+                else
+                {
+                    type = typeof(T);
+                    resultName = name;
+                }
+                T result = (T)Repo.CreateRuntimeDef(
+                    source,
+                    type,
+                    guid);
+                result.name = resultName;
+                return result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }             
+    }
+}
---
+++ VehicleReload/Reload.cs
@@ -0,0 +1,60 @@
+﻿using Base.Defs;
+using Base.Entities.Abilities;
+using Code.PhoenixPoint.Tactical.Entities.Equipments;
+using PhoenixPoint.Common.Core;
+using PhoenixPoint.Tactical.Entities.Abilities;
+using PhoenixPoint.Tactical.Entities.Equipments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleReload
+{
+    internal class Reload
+    {
+        private static readonly SharedData Shared = VehicleReloadMain.Shared;
+        private static readonly DefRepository Repo = VehicleReloadMain.Repo;
+    
+        public static void Allow_Reload()
+        {
+            GroundVehicleWeaponDef GTHailStorm = Repo.GetAllDefs<GroundVehicleWeaponDef>().FirstOrDefault(a => a.name.Equals("NJ_Armadillo_Gauss_Turret_GroundVehicleWeaponDef"));
+            TacticalItemDef GLAmmo = Repo.GetAllDefs<TacticalItemDef>().FirstOrDefault(a => a.name.Equals("PX_GrenadeLauncher_AmmoClip_ItemDef"));
+            ReloadAbilityDef Reload = Repo.GetAllDefs<ReloadAbilityDef>().FirstOrDefault(a => a.name.Equals("Reload_AbilityDef"));
+
+           // GTHailStorm.CompatibleAmmunition = new TacticalItemDef[]
+           // {
+           //     //GTHailStorm.CompatibleAmmunition[0],
+           //     PXAssRifleAmmo,
+           // };
+           //
+           // GTHailStorm.Abilities = new AbilityDef[]
+           // {
+           //     GTHailStorm.Abilities[0],
+           //     Reload,
+           // };
+
+            foreach(GroundVehicleWeaponDef weapon in Repo.GetAllDefs<GroundVehicleWeaponDef>())
+            {
+                if(weapon.Abilities != null && !weapon.Abilities.Contains(Reload))
+                {
+                    weapon.Abilities = weapon.Abilities.Append(Reload).ToArray();
+                }
+            }
+
+            foreach (GroundVehicleWeaponDef weapon in Repo.GetAllDefs<GroundVehicleWeaponDef>())
+            {   
+                if (weapon.CompatibleAmmunition == null)
+                {
+                    weapon.CompatibleAmmunition = new TacticalItemDef[]
+                    {
+                        GLAmmo,
+                    };
+                }
+                else
+                weapon.CompatibleAmmunition = weapon.CompatibleAmmunition.Append(GLAmmo).ToArray();                
+            }
+        }
+    }
+}
